9017ee49b538e6ae5798b871c8d7bd45ce4d6e09
1cbyc <isaacnsisong@gmail.com>
2024-08-03 03:38:06 +0100

    this can be tiring

---
+++ src/model_evaluation.py
@@ -0,0 +1,11 @@
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    metrics = {
+        'accuracy': accuracy_score(y_test, y_pred),
+        'precision': precision_score(y_test, y_pred),
+        'recall': recall_score(y_test, y_pred),
+        'f1_score': f1_score(y_test, y_pred)
+    }
+    return metrics
+
+# should evaluate the model's performance at least
