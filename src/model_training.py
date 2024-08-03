4a5e36ef1cb0f4fce5207aacd5814fcaf3166828
1cbyc <isaacnsisong@gmail.com>
2024-08-03 04:23:09 +0100

    this can be tiring

--- src/model_training.py
+++ src/model_training.py
@@ -10,6 +10,7 @@ def retrain_and_save_model():
     data = load_data('data/resumes_and_jobs.csv')
     processed_data = preprocess_data(data)
     X, y = create_features(processed_data)
+    
 
 def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
