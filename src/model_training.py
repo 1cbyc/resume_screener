2348a041b8dd9573f0b40be952a83422a8f33923
1cbyc <isaacnsisong@gmail.com>
2024-08-03 04:18:48 +0100

    this can be tiring

--- src/model_training.py
+++ src/model_training.py
@@ -3,7 +3,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import joblib
 import pandas as pd
-from feature_engineering import
+from feature_engineering import create_features
+from data_preprocessing import preprocess_data, load_data
+
+def retrain_and_save_model()
 
 def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
