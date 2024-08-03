32a0f95d6bc5b96f1d2921f1fd709f7de6bf56a4
1cbyc <isaacnsisong@gmail.com>
2024-08-03 04:41:13 +0100

    this can be tiring

--- src/web_app.py
+++ src/web_app.py
@@ -35,15 +35,15 @@ def rank():
     # resumes = request.json['resumes']
     # job_description = request.json['job_description']
 
-    # time to preprocess resume and job desc
-    processed_resumes = [preprocess_text(resume) for resume in resumes]
-    processed_job_description = preprocess_text(job_description)
+        # time to preprocess resume and job desc
+        processed_resumes = [preprocess_text(resume) for resume in resumes]
+        processed_job_description = preprocess_text(job_description)
 
-    job_skills = extract_skills(processed_job_description)
-    features = [[skills_match(extract_skills(resume), job_skills)] for resume in processed_resumes]
+        job_skills = extract_skills(processed_job_description)
+        features = [[skills_match(extract_skills(resume), job_skills)] for resume in processed_resumes]
 
-    predictions = model.predict(features)
-    return jsonify({'rankings': predictions.tolist()})
+        predictions = model.predict(features)
+        return jsonify({'rankings': predictions.tolist()})
 
 
 if __name__ == '__main__':
